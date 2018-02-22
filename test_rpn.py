b26668a6dbad34702ed259ff259d6931ede228f9
yichenyang96 <yyichen@umich.edu>
2018-02-22 15:59:31 -0500

    carat

--- test_rpn.py
+++ test_rpn.py
@@ -13,3 +13,6 @@ class TestBasics(unittest.TestCase):
     def test_div(self):
         result = rpn.calculate("20 4 /")
         self.assertEqual(5, result)
+    def test_carat(self):
+        result = rpn.calculate("4 3 ^")
+        self.assertEqual(64, result)
