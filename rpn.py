ac66f3c023361504374b4ab7dd29ccc54cad9f86
yichenyang96 <yyichen@umich.edu>
2018-02-22 15:56:38 -0500

    add

--- rpn.py
+++ rpn.py
@@ -1,18 +1,36 @@
-#!/usr/bin/env python3
+
+import operator
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
 
 def calculate(arg):
-	stack = list()
-	for token in arg.split():
-		try:
-			value = int(token)
-			stack.append(value)
-		except ValueError:
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			return arg1 + arg2
+    stack = list()
+    for token in arg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
+
 def main():
-	while True:
-		print(calculate(input("rpn calc> ")))
+    while True:
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
+
 if __name__ == '__main__':
-	main()
+    main()
 
--- test_rpn.py
+++ test_rpn.py
@@ -4,3 +4,12 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
+    def test_min(self):
+        result = rpn.calculate("13 1 -")
+        self.assertEqual(12, result)
+    def test_mul(self):
+        result = rpn.calculate("5 4 *")
+        self.assertEqual(20, result)
+    def test_div(self):
+        result = rpn.calculate("20 4 /")
+        self.assertEqual(5, result)
